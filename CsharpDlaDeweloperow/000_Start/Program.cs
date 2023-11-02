726a56bf3a500c2b5bdd3936fff3f98b975e2893
MaciejBalbus <maciej.albus@tlen.pl>
2023-11-02 15:15:04 +0100

    Commit

--- CsharpDlaDeweloperow/000_Start/Program.cs
+++ CsharpDlaDeweloperow/000_Start/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj swoje imie");
+
+        string? name = Console.ReadLine();
+
+        Console.WriteLine("Witaj " + name);
+
+        string jakisTekst = "jakisTekst";
+
+        char znak = 'j';
+        char znakUnicode = '\u006A';
+
+        bool czyGotowy = true;
+
+        DateTime teraz = DateTime.Now;
+        DateTime dzienUrodzin = new DateTime(1978, 9, 2);
+
+
+        byte bajt = 200;
+
+        float zmiennaFloat = 1.5f;
+        decimal zmiennaDziesietna = 1.5M;
+        double zmiennaRzeczywista = 1.5;  //nie wymaga literki double jest domyslne;
+
+        Console.WriteLine(jakisTekst);
+        Console.WriteLine(znak);
+        Console.WriteLine(znakUnicode);
+        Console.WriteLine(czyGotowy);
+        Console.WriteLine(teraz);
+        Console.WriteLine("Urodził się w: " + dzienUrodzin);
+
+        Console.WriteLine(bajt);
+        Console.WriteLine(zmiennaFloat);
+        Console.WriteLine(zmiennaDziesietna);
+        Console.WriteLine(zmiennaRzeczywista);
+
+    }
+}
---
+++ CsharpDlaDeweloperow/010_TypString/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string wiadomosc1;
+        wiadomosc1 = "jakas wiadomosc";
+        Console.WriteLine(wiadomosc1);
+
+        string? wiadomosc2 = null;
+        Console.WriteLine(wiadomosc2);
+
+        string wiadomosc3 = string.Empty;
+        Console.WriteLine(wiadomosc3);
+
+        string tekst = "On powiedzial \"Czesc\"";
+        Console.WriteLine(tekst);
+
+        string katalogWindows = "C:\\windows\\i_takDalej\\i_takDalej";
+        Console.WriteLine(katalogWindows);
+
+        string fontsFolder = @"D:\root\fonts";
+        Console.WriteLine(fontsFolder);
+
+        string rezultatConcat = string.Concat(tekst, " dopisany string", " kolejny string");
+        Console.WriteLine(rezultatConcat);
+
+        string rezultatConcat2 = tekst + " dopisany string" + " kolejny string";
+        Console.WriteLine(rezultatConcat2);
+
+        string interpolowany = $"{tekst} i dalsza czesc";
+        Console.WriteLine(interpolowany);
+
+        StringBuilder strBuilder = new StringBuilder("Poczatek");
+        strBuilder.Append(" dodano");
+        strBuilder.Append(" i znowu dodano");
+        strBuilder.Append(" i jeszcze dodano");
+        Console.WriteLine(strBuilder);
+        //lub 
+        string rezultat = strBuilder.ToString();
+        Console.WriteLine(rezultat);
+
+
+
+    }
+}
